a89038d1bc919c63eb8e1ff3f200c58197304832
samuelribeiroo <samuelribeirodev@gmail.com>
2024-04-14 12:04:01 -0300

    add: passed on jest

--- calculator-js/calculator.js
+++ calculator-js/calculator.js
@@ -1,37 +1,71 @@
 const log = console.log;
 
-export class Calculator {
-  constructor() {
-    this.result = 0;
+// Solução que passou no teste
+function calculator(x, y, operator) {
+  let result;
+
+  switch (operator) {
+    case "+":
+      result = x + y;
+      break;
+
+    case "-":
+      result = x - y;
+      break;
+
+    case "*":
+      result = x * y;
+      break;
+
+    case "/":
+      result = x / y;
+      break;
+    default:
+      throw new Error("Operador selecionado não existe.");
   }
 
-  getSum(x, y) {
-    this.result = x + y;
-
-    return this.result;
-  }
-
-  getMinus(x, y) {
-    this.result = x - y;
-
-    return this.result;
-  }
-
-  getMultiply(x, y) {
-    this.result = x * y;
-
-    return this.result;
-  }
-
-  getDivide(x, y) {
-    const checkInput = y !== 0 ? x / y : "Divisão igual a zero.";
-
-    this.result = checkInput
-
-    return this.result
-  }
+  return result;
 }
 
-const operators = new Calculator();
+const operationTest = calculator(10, 25, "*");
 
-log(operators.getDivide(50, 0));
+log(operationTest);
+
+module.export = calculator;
+
+// Minha solução, implementando SRP - (Single Responsability Principle)
+// export class Calculator {
+//   constructor() {
+//     this.result = 0;
+//   }
+
+//   getSum(x, y) {
+//     this.result = x + y;
+
+//     return this.result;
+//   }
+
+//   getMinus(x, y) {
+//     this.result = x - y;
+
+//     return this.result;
+//   }
+
+//   getMultiply(x, y) {
+//     this.result = x * y;
+
+//     return this.result;
+//   }
+
+//   getDivide(x, y) {
+//     const checkInput = y !== 0 ? x / y : "Divisão igual a zero.";
+
+//     this.result = checkInput;
+
+//     return this.result;
+//   }
+// }
+
+// const operators = new Calculator();
+
+// log(operators.getDivide(50, 0));
