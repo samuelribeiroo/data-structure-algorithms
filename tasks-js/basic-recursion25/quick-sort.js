6e365e3ef24950e2649ea3ae3842473345b077b5
samuelribeiroo <samuelribeirodev@gmail.com>
2024-06-11 23:21:21 -0300

    initial concept bout recursion

---
+++ tasks-js/basic-recursion25/quick-sort.js
@@ -0,0 +1,39 @@
+function quickSort(arr) {
+  if (arr.length <= 1) {
+    return arr;
+  }
+
+  const pointReference = arr[arr.length - 1];
+  const axisLeft = [];
+  const axisRight = [];
+
+  for (let index = 0; index < arr.length - 1; index++) {
+    const ordenedIndex = arr[index];
+
+    if (ordenedIndex < pointReference) {
+      axisLeft.push(ordenedIndex);
+    } else {
+      axisRight.push(ordenedIndex);
+    }
+  }
+
+  return [...quickSort(axisLeft), pointReference, ...quickSort(axisRight)];
+}
+
+const numbersList = [1];
+
+const sortedNumbers = quickSort(numbersList);
+
+module.exports = quickSort;
+
+function counting(number = 'Parametro não fornecido.') {
+  console.log(number);
+
+  let baseCase = number < 10 ? counting(number + 1) : null
+  
+  return baseCase
+}
+
+counting(1)
+console.log('=============================')
+counting(7)
