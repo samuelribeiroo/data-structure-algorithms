557ea203983886a0d5ac621acde95eaa562442ed
samuelribeiroo <samuelribeirodev@gmail.com>
2024-07-19 15:18:34 -0300

    add(array prototypes reviews): .concat, .slice

---
+++ tasks-js/find-min31/find-min.js
@@ -0,0 +1,30 @@
+function sortArray(array) {
+  const length = array.length;
+  let sortedArray = []
+
+  for (let index = 1; index < length; index++) {
+    const minIndex = findMinIndex(array);
+     sortedArray = sortedArray.concat(array.slice(minIndex, minIndex + 1));
+     console.log(sortedArray)
+     array = array.slice(0, minIndex).concat(array.slice(minIndex + 1));
+  }
+
+  return sortedArray
+}
+
+function findMinIndex(array) {
+  const length = array.length;
+  let minIndex = 0;
+
+  for (let index = 1; index < length; index++) {
+    if (array[index] < array[minIndex]) {
+      minIndex = index;
+    }
+  }
+
+  return minIndex;
+}
+
+const list = [20, 30, 40, 1];
+const sortedList = sortArray(list);
+console.log(sortedList); 
